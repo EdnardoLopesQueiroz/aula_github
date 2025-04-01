8a730cf404c1b37ddeac0014f173e5039a5eea6c
ednardolopesqueiroze@gmail.com <testeDanilo@hotmail.com>
2025-04-01 17:30:02 -0300

    initial commit

---
+++ aula.revisao.py
@@ -0,0 +1,108 @@
+
+
+
+
+# num = int(input("Insira um numero em metros para converter para cm: "))
+
+# cm = num * 100
+
+# print (" os metros convertidos em cm é: ", cm)
+
+
+
+# alt = float(input("vamos calcular a Área de um retangulo, insira a ALTURA: "))
+# larg = float(input("Insira a Largura: "))
+
+# area = alt * larg
+
+# print(" A Área é: ", area)
+
+
+# alt = float(input("Vamos calcular o IMC, digite a sua ALTURA: "))
+# peso = float (input("Digite o peso: "))
+
+# imc = peso / alt**2
+
+# print(f"o seu IMC é {imc: .2f}")
+
+
+# capital =float(input("Digite o capital: "))
+# taxa = float(input("Digite a Taxa de Juros"))
+# temp = int(input("Digite o tempo de aplicação"))
+
+# juros = capital * taxa * temp
+
+# print(f"o Valor de Juros é {juros}")
+# __________________________________________________________________________
+# not1 = float(input("Vamos calcular a média do Aluno, digite a nota 1: "))
+# not2 = float(input("Digite a nota 2: "))
+# not3 = float(input("Digite a nota 3: "))
+# not4 = float(input("Digite a nota 4: "))
+
+# media = (not1 + not2 + not3 + not4) / 4
+
+# if media >= 6.98:
+#     print(f"Parabens foi foi aprovado!!! {media: .2f}")
+# else:
+#     print("Reprovado!!!")
+
+
+
+# preco = float(input("Insira o valor do produto: "))
+# desc = 0.1
+
+# preco_desc = preco - (preco * 0.1)
+
+# print("O preço do produto com 10% de desconto é", preco_desc)
+
+
+
+
+# num = int(input("Insira sua nota de 0 a 100 para saber a Classificação: "))
+
+# if num <= 20:
+#     print("Nota Categoria F")
+# elif num <= 40:
+#     print("Nota Categoria D")
+# elif num <= 60:
+#     print("Nota Categoria C")
+# elif num <= 80:
+#     print("Nota Categoria B")
+# elif num <= 100:
+#     print("Nota Categoria A")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
